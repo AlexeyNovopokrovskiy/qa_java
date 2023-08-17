cb25c818688ddb60eebd634a69c69c8d02457eb7
Alexey Novopokrovkiy <alexeydracula@yandex.ru>
2023-08-17 19:45:51 +0300

    1st commit

--- src/test/java/com/example/FelineTest.java
+++ src/test/java/com/example/FelineTest.java
@@ -1,4 +1,47 @@
+package com.example;
+
+
+
 import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
+
+
 public class FelineTest {
-  
+
+
+    @Test
+    public void eatMeat() throws Exception {
+        Feline cats = new Feline();
+        List<String> expectedResult = List.of("Animals", "Birds", "Fish");
+        List<String> result = cats.eatMeat();
+        assertEquals(result,expectedResult);
+    }
+    @Test
+    public void getFamily(){
+        Feline cats = new Feline();
+        String expectedResult = "Feline";
+        String result = cats.getFamily();
+        assertEquals(result,expectedResult);
+    }
+    @Test
+    public void getKittens(){
+        Feline cats = new Feline();
+        int result = cats.getKittens();
+        assertEquals(result,1);
+    }
+    @Test
+    public void getKittensX(){
+        Feline cats = new Feline();
+        int x = 10;
+        int result = cats.getKittens(x);
+        assertEquals(result,x);
+    }
 }
--- src/test/java/com/example/LionTest.java
+++ src/test/java/com/example/LionTest.java
@@ -1,4 +1,149 @@
+package com.example;
+
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.junit.runners.Parameterized;
+
+import java.util.List;
+
+@RunWith(Parameterized.class)
 public class LionTest {
-  
+    @Parameterized.Parameter
+    public String sex;
+    @Parameterized.Parameter(1)
+    public boolean mane;
+
+
+
+    @Parameterized.Parameters // добавили аннотацию
+    public static Object[][] param() {
+        return new Object[][]{
+                {"Самец",true},
+                {"Самка",false},
+                {"else",false}
+        };
+    }
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void getKittens() {
+        Feline fel = Mockito.mock(Feline.class);
+        Lion savannah;
+
+        {
+            try {
+                savannah = new Lion(sex, fel);
+
+                Mockito.when(savannah.getKittens()).thenReturn(1);
+                assertEquals(savannah.getKittens(),1);
+
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+
+    }
+
+    @Test
+    public void doesHaveMane() {
+        Feline fel = Mockito.mock(Feline.class);
+        Lion savannah;
+
+        {
+            try {
+                savannah = new Lion(sex, fel);
+                assertEquals(savannah.doesHaveMane(), mane);
+
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+
+    }
+
+    @Test
+    public void getFood() {
+        Feline fel = Mockito.mock(Feline.class);
+        Lion savannah;
+
+
+        try {
+            savannah = new Lion(sex, fel);
+
+            Mockito.when(fel.getFood("Хищник")).thenReturn(List.of("Animals", "Birds", "Fish"));
+            List<String> expectedResult = List.of("Animals", "Birds", "Fish");
+            List<String> result = savannah.getFood();
+            assertEquals(result, expectedResult);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+/*
+ @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void getKittens() {
+        Feline spyFel = Mockito.spy(new Feline());
+        Lion savannah;
+
+        {
+            try {
+                savannah = new Lion(sex, spyFel);
+                assertEquals(savannah.getKittens(),1);
+
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+
+    }
+
+    @Test
+    public void doesHaveMane() {
+        Feline spyFel = Mockito.spy(new Feline());
+        Lion savannah;
+
+        {
+            try {
+                savannah = new Lion(sex, spyFel);
+                assertEquals(savannah.doesHaveMane(), mane);
+
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+
+    }
+
+    @Test
+    public void getFood() {
+        Feline spyFel = Mockito.spy(new Feline());
+        Lion savannah;
+
+
+        try {
+        savannah = new Lion(sex, spyFel);
+            List<String> expectedResult = List.of("Животные", "Птицы", "Рыба");
+            List<String> result = savannah.getFood();
+            assertEquals(result, expectedResult);
+            //System.out.println(savannah.getFood());
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+*/
+
+
 }
